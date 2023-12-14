9814275edaa11671a21f75d204300d430d67f943
kumhee <kumheee95@gmail.com>
2023-12-14 18:09:25 +0900

    FEAT:도서정보 물러오는 기능 추가

--- ex04/app/src/main/java/com/example/ex04/BookActivity.java
+++ ex04/app/src/main/java/com/example/ex04/BookActivity.java
@@ -3,13 +3,27 @@ package com.example.ex04;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,6 +31,10 @@ import java.util.Objects;
 
 public class BookActivity extends AppCompatActivity {
     List<HashMap<String, Object>> list = new ArrayList<>();
+
+    BookAdapter adapter;
+    String query = "안드로이드";
+    int page = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +45,15 @@ public class BookActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         new KakaoThread().execute();
+
+        //더보기 버튼 클릭했을 때 1페이지 증가
+        findViewById(R.id.more).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                page += 1;
+                new KakaoThread().execute();
+            }
+        });
     }
 
     //카카오API를 호출하기 위한 Thread
@@ -34,7 +61,7 @@ public class BookActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... strings) {
-            String url = "https://dapi.kakao.com/v3/search/book?target=title&query=안드로이드";
+            String url = "https://dapi.kakao.com/v3/search/book?target=title&query=" + query + "&page=" + page;
             String result = KakaoAPI.connect(url);
             System.out.println("............." + result);
             return result;
@@ -45,8 +72,12 @@ public class BookActivity extends AppCompatActivity {
             super.onPostExecute(s);
             bookParser(s);
             System.out.println("...........데이터갯수:" + list.size());
+            adapter = new BookAdapter();
+            ListView listView = findViewById(R.id.list);
+            listView.setAdapter(adapter);
         }
 
+        //결과 파싱
         public void bookParser(String result) {
             try {
                 JSONArray array = new JSONObject(result).getJSONArray("documents");
@@ -75,4 +106,110 @@ public class BookActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    //어뎁터 생성
+    class BookAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.item_book, parent, false);
+            HashMap<String, Object> map = list.get(position);
+
+            TextView title = convertView.findViewById(R.id.title);
+            title.setText(map.get("title").toString());
+            title.setTypeface(null, Typeface.BOLD);
+
+            int intPrice = Integer.parseInt(map.get("price").toString());
+            DecimalFormat df = new DecimalFormat("#,###원");
+            TextView price = convertView.findViewById(R.id.price);
+            price.setText(df.format(intPrice));
+
+            TextView authors = convertView.findViewById(R.id.authors);
+            authors.setText(map.get("authors").toString());
+
+            ImageView image = convertView.findViewById(R.id.image);
+            String strImage = map.get("image").toString();
+            if(strImage.equals("")) {
+                image.setImageResource(R.drawable.book);
+            } else {
+                Picasso.with(BookActivity.this)
+                        .load(map.get("image").toString())
+                        .into(image);
+            }
+
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    View view = (View)View.inflate(BookActivity.this, R.layout.detail_book, null);
+                    TextView title = view.findViewById(R.id.title);
+                    title.setText(map.get("title").toString());
+
+                    int intPrice = Integer.parseInt(map.get("price").toString());
+                    DecimalFormat df = new DecimalFormat("#,###원");
+                    TextView price = view.findViewById(R.id.price);
+                    price.setText(df.format(intPrice));
+
+                    TextView authors = view.findViewById(R.id.authors);
+                    authors.setText(map.get("authors").toString());
+
+                    TextView contents = view.findViewById(R.id.contents);
+                    contents.setText(map.get("contents").toString());
+
+                    ImageView image = view.findViewById(R.id.image);
+                    String strImage = map.get("image").toString();
+                    if(strImage.equals("")) {
+                        image.setImageResource(R.drawable.book);
+                    } else {
+                        Picasso.with(BookActivity.this)
+                                .load(map.get("image").toString())
+                                .into(image);
+                    }
+
+                    new AlertDialog.Builder(BookActivity.this)
+                            .setTitle("도서정보")
+                            .setView(view)
+                            .setPositiveButton("확인", null)
+                            .show();
+                }
+            });
+            return convertView;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.book, menu);
+
+        SearchView searchView = (SearchView)menu.findItem(R.id.search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String str) {
+                query = str;
+                page = 1;
+                list = new ArrayList<>();
+                new KakaoThread().execute();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
 }
